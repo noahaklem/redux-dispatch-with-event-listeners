e46621d8e4c305928688134533378375e9bf294a
Noah A Klem <noahaklem@gmail.com>
2021-11-27 12:34:37 -0700

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,27 @@
 // add code snippets from README
+let state;
+
+function reducer(state = {count: 0}, action){
+  switch(action.type) {
+    case "INCREASE_COUNT":
+      return {count: state.count + 1};
+    default:
+      return state
+  } 
+}
+
+function dispatch(action){
+  state = reducer(state, action);
+  render();
+}
+
+function render(){
+  document.getElementById('container'). textContent = state.count
+}
+dispatch({type: '@@init'})
+
+let button = document.getElementById('button');
+
+button.addEventListener('click', () => {
+  dispatch({type: 'INCREASE_COUNT'})
+})
